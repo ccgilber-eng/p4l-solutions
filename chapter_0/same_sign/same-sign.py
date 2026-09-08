5fd2c3e0f09bbedcf0223f9f238a69b06bc25866
ccgilber-eng <ccgilber@andrew.cmu.edu>
2026-09-07 20:31:02 -0400

    Complete SameSign in python

---
+++ chapter_0/same_sign/same-sign.py
@@ -0,0 +1,15 @@
+# Insert your same_sign() function here.
+def same_sign(x: int, y: int) -> bool:
+    """
+    Determine whether two integers have the same sign.
+    Args:
+        x: First integer.
+        y: Second integer.
+    Returns:
+        True if x and y share the same sign (including both zero), False otherwise.
+    """
+    if x*y>=0:
+        return True
+    else: 
+        return False
+        
