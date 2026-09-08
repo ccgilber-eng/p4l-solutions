426a7687571d546576a3d207c52c6dce0c167d2a
ccgilber-eng <ccgilber@andrew.cmu.edu>
2026-09-08 17:31:51 -0400

    Complete CrossOffMultiples in python

---
+++ chapter_0/cross_off_multiples/cross-off-multiples.py
@@ -0,0 +1,19 @@
+# Insert your cross_off_multiples() function here, along with any subroutines that you need.
+def cross_off_multiples(prime_booleans: list[bool], p:int) -> list[bool]:
+    """
+    Returns an updated list in which all variables in the array whose indices are multiples of p (greater than p) have
+    been set to false.
+    Parameters:
+    - prime_booleans (list): a list of boolean variables storing the primality of each nonnegative integer
+    - p (int): an integer
+    Returns:
+    list (bool): a list of boolean variables storing the primality of each nonnegative integer up to and including n with
+    multiples of p (greater than p) set to false.
+    """
+    n=len(prime_booleans)-1
+
+    for i in range(2*p, n+1, p):
+        prime_booleans[i]=False
+
+
+    return prime_booleans
