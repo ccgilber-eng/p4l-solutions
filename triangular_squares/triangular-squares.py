82ad6c9b8b294d29693dfdfcc678115902a1c768
ccgilber-eng <ccgilber@andrew.cmu.edu>
2026-09-20 20:03:10 -0400

    Complete TriangularSquares in python

---
+++ triangular_squares/triangular-squares.py
@@ -0,0 +1,46 @@
+import sys
+import math 
+
+# Please do not remove package declarations because these are used by the autograder. 
+# If you need additional packages, then you may declare them above.
+
+
+# Insert your triandsq(n) function here, along with any subroutines that you need.
+# The function should return a list of triangular and square numbers under n.
+
+def triandsq(n: int) -> list:
+
+    triangle_sq = []
+    for i in range(1, n):
+        if is_perfect_square(i) == True:
+            if is_triangular(i) == True:
+                triangle_sq.append(i)
+    
+    return triangle_sq 
+
+
+
+def is_triangular(i):
+    current_value = 0
+    a = 1
+    while current_value < i:
+        current_value += a
+        a += 1
+    
+    if current_value == i:
+        return True
+    else:
+        return False 
+
+
+def is_perfect_square(i):
+    root = math.isqrt(i)
+    if root**2 == i:
+        return True
+    else:
+        return False
+
+
+
+
+
